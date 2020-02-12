a6c496eb784fef329e79675124f04f4aaf2f6025
linfeng guan <928497141@qq.com>
2020-02-12 17:12:05 +0000

    add

---
+++ DBUtility/SQLHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBUtility
+{
+    /// <summary>
+    /// Common access classes that operate on SQL server databases
+    /// </summary>
+    class SQLHelper
+    {
+    }
+}
