e5d14241072e61e51a74a2a37eba7ae29047ee95
linfeng guan <928497141@qq.com>
2020-03-08 20:14:13 +0000

    sql

--- DBUtility/SQLHelper.cs
+++ DBUtility/SQLHelper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DBUtility
 {
@@ -15,5 +17,32 @@ namespace DBUtility
         //Connection string 
         private static string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
 
+        #region Standard SQL Statements 
+
+        //Implementation of the deletion of the database and other changes
+        public static int Update(string sql)
+        {
+            //Instantiate a connection
+            SqlConnection conn = new SqlConnection(connString);
+            //Instantiate Commnad
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            //Execute and return results 
+            try
+            {
+                //Open the Database
+                conn.Open();
+                //Returns the result, the return value is the number of rows affected, the int type
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                //Close a database connection 
+                conn.Close();
+            }
+        }
     }
 }
