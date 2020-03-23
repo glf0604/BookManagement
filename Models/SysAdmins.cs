0f3299b90e45cb8441ff4aa909d89a1ac3ca5ed9
linfeng guan <928497141@qq.com>
2020-03-23 19:40:46 +0000

    Sysadmins.cs

--- Models/SysAdmins.cs
+++ Models/SysAdmins.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    class SysAdmins
+    /// <summary>
+    /// The entity class used to represent the logged-on user
+    /// </summary>
+    public class SysAdmins
     {
+        public int LoginId { get; set; }   //LoginId
+        public string LoginPwd { get; set; }   //LoginPwd
+        public string UserName { get; set; }   //UserName 
+        public bool IsDisable { get; set; }   //IsDisable
+        public bool IsSuperUser { get; set; }   //IsSuperUser 
+        public DateTime LastLoginTime { get; set; }   //LastLoginTime
     }
 }
