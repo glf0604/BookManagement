5cbcc781bee4c64d8bf57ba2635846ca51c8ec6d
linfeng guan <928497141@qq.com>
2020-03-25 15:21:55 +0000

    Common

--- Common/ValidateInput.cs
+++ Common/ValidateInput.cs
@@ -24,6 +24,17 @@ namespace Common
             Regex objRegex = new Regex(@"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
             return objRegex.IsMatch(txt);
         }
-       
+        //Whether it's a telephone? 
+        public static bool IsMobile(string txt)
+        {
+            Regex objRegex = new Regex(@"^[1][3578]\d{9}$");
+            return objRegex.IsMatch(txt);
+        }
+        //Whether it is Chinese characters?
+        public static bool IsChinese(string txt)
+        {
+            Regex objRegex = new Regex(@"^[\u4e00-\u9fa5]{0,}$");
+            return objRegex.IsMatch(txt);
+        }
     }
 }
