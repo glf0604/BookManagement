914c2cfdd5f77d9a20c3aaa6496cf93e265b7258
linfeng guan <928497141@qq.com>
2020-03-25 14:54:13 +0000

    Get all category information

---
+++ DAL/BookTypeServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DBUtility;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAL
+{
+    /// <summary>
+    /// Methods of operation for book types
+    /// </summary>
+    public class BookTypeServices
+    {
+        public DataTable GetBookType()
+        {
+            string sql = "Select TypeId,TypeName,ParentTypeId,TypeDESC from BookType";
+            try
+            {
+                SqlDataReader objReader = SQLHelper.GetReader(sql);
+                DataTable dt = new DataTable();
+                dt.Load(objReader);
+                return dt;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
