1d57d3b3a987e2fbf62b8ede727c92fca2cc1a1a
linfeng guan <928497141@qq.com>
2020-03-26 16:46:32 +0000

    member

--- DAL/MemberServices.cs
+++ DAL/MemberServices.cs
@@ -113,5 +113,65 @@ namespace DAL
                 throw ex;
             }
         }
+        //Get the details of a member with a membership card
+        public Member GetMemberByCardId(string memberCardId)
+        {
+            //Preparing SQL statements
+            string sql = "Select MemberId, MemberName, MemberCardId, MemberLevel, IdType, IdNumber, Gender, TelNo, Birthday, HomeAddress, MemberPhoto, CardStatus,";
+            sql += " CardClosingDate, IsReturnDeposit, PayMethod, LoginId, OperatingTime, ReMarks from Member  Where MemberCardId=@MemberCardId ";
+
+
+            //Preparing parameters in SQL statements
+            SqlParameter[] para = new SqlParameter[]
+            {
+                new SqlParameter("@MemberCardId",memberCardId),
+            };
+
+            //Execute and return results
+            try
+            {
+                //Receive return values using SQLDataReader
+                SqlDataReader objReader = SQLHelper.GetReader(sql, para);
+                //Determine if it is empty
+                if (!objReader.HasRows) return null;
+                //Instantiate Member Object
+                Member objMember = new Member();
+                //Read
+                if (objReader.Read())
+                {
+                    objMember = new Member()
+                    {
+                        MemberId = objReader["MemberId"].ToString(),
+                        MemberName = objReader["MemberName"].ToString(),
+                        MemberCardId = objReader["MemberCardId"].ToString(),
+                        MemberLevel = Convert.ToInt32(objReader["MemberLevel"]),
+                        IdType = objReader["IdType"].ToString(),
+                        IdNumber = objReader["IdNumber"].ToString(),
+                        Gender = objReader["Gender"].ToString(),
+                        TelNo = objReader["TelNo"].ToString(),
+                        Birthday = Convert.ToDateTime(objReader["Birthday"]),
+                        HomeAddress = objReader["HomeAddress"].ToString(),
+                        MemberPhoto = objReader["MemberPhoto"].ToString(),
+                        CardStatus = objReader["CardStatus"].ToString(),
+                        CardClosingDate = Convert.ToDateTime(objReader["CardClosingDate"]),
+                        IsReturnDeposit = Convert.ToBoolean(objReader["IsReturnDeposit"]),
+                        PayMethod = objReader["PayMethod"].ToString(),
+                        LoginId = Convert.ToInt32(objReader["LoginId"]),
+                        OperatingTime = Convert.ToDateTime(objReader["OperatingTime"]),
+                        ReMarks = objReader["ReMarks"].ToString(),
+                    };
+                }
+                //Close read
+                objReader.Close();
+                //return
+                return objMember;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
     }
 }
