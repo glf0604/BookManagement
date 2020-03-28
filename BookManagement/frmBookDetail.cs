6e3b89d011dae39fbc6e340877bc33ae17105f27
linfeng guan <928497141@qq.com>
2020-03-28 17:37:31 +0000

    Book detail

--- BookManagement/frmBookDetail.cs
+++ BookManagement/frmBookDetail.cs
@@ -17,9 +17,32 @@ namespace BookManagement
 {
     public partial class frmBookDetail : Form
     {
+        //Instantiate the Operation class of the publishing house
+        private BookPressServices objBookPressServices = new BookPressServices();
+        //The Operation class of instantiated books
+        private BookServices objBookServices = new BookServices();
+        //Operational classes that instantiate book categories
+        private BookTypeServices objBookTypeServices = new BookTypeServices();
+        //Define the Global book publishing house List initialization drop-down box
+        private List<BookPress> objListPress = new List<BookPress>();
+        //Define a global book category (Level I) List initialization drop-down box
+        private List<BookType> objListTypeOne = new List<BookType>();
+        //Define a global book category (level two) List initialization drop-down box
+        private List<BookType> objListTypeTwo = new List<BookType>();
+        //Define an action identity variable
+        private int actionFlag = 0; //2----Add    3----Modify
+        //Define a camera operation class
+        private Video objVideo = null;
+
         public frmBookDetail()
         {
             InitializeComponent();
+
+            //Initialize drop-down box data
+            LoadDropListInfo();
+
+            //Set BookID to Empty
+            lblBookId.Text = string.Empty;
         }
     }
 }
