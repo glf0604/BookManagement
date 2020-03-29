ea4d8678867f8ccb3cd31571cc7b256e017456d5
linfeng guan <928497141@qq.com>
2020-03-29 22:27:56 +0100

    type

--- BookManagement/frmBookType.cs
+++ BookManagement/frmBookType.cs
@@ -15,9 +15,49 @@ namespace BookManagement
 {
     public partial class frmBookType : Form
     {
+        //Instantiate a DataTable object to store all category information
+        private DataTable dt = new DataTable();
+        //Instantiate an object for an Booktype operation
+        private BookTypeServices objBookTypeServices = new BookTypeServices();
+        //Defines a global operation Flag 
+        private int actionFlag = 0;  //1--add   2--modify
+
         public frmBookType()
         {
             InitializeComponent();
+
+            //Disable Detail box 
+            gboxNodeDetial.Enabled = false;
+            //Used to initialize the DT
+            try
+            {
+                dt = objBookTypeServices.GetBookType();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error getting information about book categories! Specific errors:" + ex.Message, "System Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            //Judging by the results.
+            if (dt.Rows.Count == 0)
+            {
+                //Disable button
+                btnAddRootNode.Enabled = true;
+                btnAddSubNode.Enabled = false;
+                btnUpdateNode.Enabled = false;
+                btnDeleteNode.Enabled = false;
+            }
+            else
+            {
+                //Disable button
+                btnAddRootNode.Enabled = false;
+                btnAddSubNode.Enabled = true;
+                btnUpdateNode.Enabled = true;
+                btnDeleteNode.Enabled = true;
+
+                //Initialization of TreeView 
+                LoadBookType();
+            }
         }
     }
 }
