1001c93191fe41dd172926c103c7a0e9fe201aee
linfeng guan <928497141@qq.com>
2020-03-30 21:10:03 +0100

    MD

--- BookManagement/frmMemberDetail.cs
+++ BookManagement/frmMemberDetail.cs
@@ -7,11 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Models;
+using DAL;
+using Common;
+using MyVideo;
 
 namespace BookManagement
 {
     public partial class frmMemberDetail : Form
     {
+        //Instantiate member Action Class 
+        private MemberServices objMemberServices = new MemberServices();
+        //Instantiate the action class for the membership level 
+        private MemberLevelServices objMemberLevelServices = new MemberLevelServices();
+        //Ways to instantiate user logon
+        private SysAdminsServices objSysAdminsServices = new SysAdminsServices();
+        //Define local action identifiers
+        private int actionFlag = 0;
+        //Define Camera manipulation classes
+        private Video objVideo = null;
+
         public frmMemberDetail()
         {
             InitializeComponent();
