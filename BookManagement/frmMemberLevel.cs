f919377b9985a24d95a1111e5663ab63c6b902c6
linfeng guan <928497141@qq.com>
2020-03-31 18:27:14 +0100

    ML

--- BookManagement/frmMemberLevel.cs
+++ BookManagement/frmMemberLevel.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Models;
+using DAL;
 
 namespace BookManagement
 {
     public partial class frmMemberLevel : Form
     {
+        //Instantiate a List<MemberLevel>and store all The memberlevel information</MemberLevel>
+        private List<MemberLevel> objListLevel = new List<MemberLevel>();
+        //An operational method class that instantiates a member level
+        private MemberLevelServices objMemberLevelServices = new MemberLevelServices();
+        //Define a representation of an action
+        private int actionFlag = 0; //1--Add  2--Modify 
+
         public frmMemberLevel()
         {
             InitializeComponent();
+            //Disable Detail area
+            gboxMemberLevel.Enabled = false;
+            //Load member level data to Listview display
+            LoadMemberLevelInfo();
         }
     }
 }
