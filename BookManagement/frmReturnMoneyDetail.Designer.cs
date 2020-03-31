29124aa41405bcf1b0cf2e5a4eb73b971cb3f535
linfeng guan <928497141@qq.com>
2020-03-31 18:50:30 +0100

    frmReturnMoneyDetail

--- BookManagement/frmReturnMoneyDetail.Designer.cs
+++ BookManagement/frmReturnMoneyDetail.Designer.cs
@@ -458,6 +458,7 @@
             this.btnClose.TabIndex = 128;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // pbCurrentBook
             // 
