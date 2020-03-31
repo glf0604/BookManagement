17d17a66d0e905a46b4ebc2bc613566a0f10e383
linfeng guan <928497141@qq.com>
2020-03-31 18:37:13 +0100

    RB

--- BookManagement/frmReturnBook.cs
+++ BookManagement/frmReturnBook.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Models;
+using DAL;
+using Common;
 
 namespace BookManagement
 {
     public partial class frmReturnBook : Form
     {
+        //Instantiate Expense detail form 
+        public static frmReturnMoneyDetail objFrmReturnMoneyDetail = null;
+        //Define a form for a member detail
+        public static frmMemberDetail objFrmMemberDetail = null;
+        //Define the class for the current user 
+        private Member objMember = null;
+        //Define a BorrowId
+        private string borrowId = string.Empty;
+        //Instantiate member Action Class
+        private MemberServices objMemberServices = new MemberServices();
+        //Instantiate an action class at a member level
+        private MemberLevelServices objMemberLevelServices = new MemberLevelServices();
+        //Instantiation of a BorrowBookServices operation class
+        private BorrowBookServices objBorrowBookServices = new BorrowBookServices();
+        //Instantiation of a Detail operation class
+        private BorrowBookDetailServices objBorrowBookDetailServices = new BorrowBookDetailServices();
+        //Define a DataTable current member book library details
+        private DataTable dt = null;
+        //Instantiation of a book operation class
+        private BookServices objBookServices = new BookServices();
+        //Define a page for book detail viewing
+        public static frmBookDetail objFrmBookDetail = null;
+
         public frmReturnBook()
         {
             InitializeComponent();
+            //Initialization of DataGridview 
+            dgvReturn.AutoGenerateColumns = false;
         }
     }
 }
