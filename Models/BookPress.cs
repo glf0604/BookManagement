e1eb8623693c382c0f7f8084b1e7db71347bc8a4
linfeng guan <928497141@qq.com>
2020-04-03 22:02:43 +0100

    models book press

--- Models/BookPress.cs
+++ Models/BookPress.cs
@@ -9,11 +9,12 @@ namespace Models
     /// <summary>
     /// The entity class used to represent the publishing house information
     /// </summary>
+
     public class BookPress
     {
-        public int PressId { get; set; }   //press id
+        public int PressId { get; set; }   //press Id
         public string PressName { get; set; }   //Press Name
-        public string PressTel { get; set; }   //Press Tel
+        public string PressTel { get; set; }   //Press Telephone
         public string PressContact { get; set; }   //Press Contact
         public string PressAddress { get; set; }   //Press Address
     }
