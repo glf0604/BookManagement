e35c4ad121e2ab995867667569d65d5ed0f1c698
linfeng guan <928497141@qq.com>
2020-04-13 21:02:46 +0100

    BorrowBookServices

--- DAL/BorrowBookServices.cs
+++ DAL/BorrowBookServices.cs
@@ -90,6 +90,7 @@ namespace DAL
             {
                 new SqlParameter("@MemberId",memberId),
             };
+            
             //Execute and return
             try
             {
