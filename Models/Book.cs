07b021b3901cd191f060fadc51ef4bf2fc352464
linfeng guan <928497141@qq.com>
2020-04-14 21:07:38 +0100

    Models Book

--- Models/Book.cs
+++ Models/Book.cs
@@ -17,7 +17,7 @@ namespace Models
         public int BookType { get; set; } //Book type
         public string ISBN { get; set; } //ISBN
         public string BookAuthor { get; set; } //Book author 
-        public double BookPrice { get; set; } //book price
+        public double BookPrice { get; set; } //Book price
         public int BookPress { get; set; } //book press
         public string BookImage { get; set; } //book picture
         public DateTime BookPublishDate { get; set; } //book publish date 
