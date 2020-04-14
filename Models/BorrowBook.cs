d18465b3f16424a2fdb65bf308d68c2e1f46c6c5
linfeng guan <928497141@qq.com>
2020-04-14 21:20:26 +0100

    Models BorrowBook

--- Models/BorrowBook.cs
+++ Models/BorrowBook.cs
@@ -13,7 +13,7 @@ namespace Models
     public class BorrowBook
     {
         public string BorrowId { get; set; }  //BorrowId---Primary Key
-        public string MemberId { get; set; }   //Member Id
+        public string MemberId { get; set; }  //Member Id
         public int BorrowedNum { get; set; }  //Borrowed Number
         public int OverdueNum { get; set; }   //Overdue Number
     }
